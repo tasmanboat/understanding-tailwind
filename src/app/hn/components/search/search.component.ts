aadda36945cd0797edf94077302cd479967290c9
noname <you@example.com>
2022-05-13 18:48:14 +0800

    update search.component.ts

--- src/app/hn/components/search/search.component.ts
+++ src/app/hn/components/search/search.component.ts
@@ -53,7 +53,9 @@ export class SearchComponent implements OnInit {
 // #region load the component when page refreshes, handling route param
   ngOnInit(): void {
     this.keyword = this.route.snapshot.queryParamMap.get('keyword') ?? 'today';
-    this.page = Number(this.route.snapshot.queryParamMap.get('page') ?? '1'); // to add the query param to the first page
+    this.page = Number(this.route.snapshot.queryParamMap.get('page') ?? '1'); // effect1: to read the query param on any page
+    // effect2: to add the query param to the first page
+
     // this.queryResult$ = this.searchApiService.getQueryResult(this.keyword.trim());
     this.queryResult$ = this.getQueryResult(this.keyword);
     console.log(`(SearchComponent) ngOnInit`);
